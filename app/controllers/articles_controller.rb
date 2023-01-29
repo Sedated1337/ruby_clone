cdf5952d0d39af5fce4a27fe66c441f9356495c3
Sedated1337 <vredbi123@gmail.com>
2023-01-29 03:20:50 +0200

    add header menu

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,19 @@
+class ArticlesController < ApplicationController
+  def recipes
+    render 'welcome/index'
+  end
+
+  def advices
+    render 'welcome/index'
+  
+  end
+
+  def news
+    render 'welcome/index'
+  end  
+
+  def donate
+    render 'welcome/index'
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root "welcome#index"
-  get 'about', to: 'welcome#about'
+
+  get 'news', to: 'articles#news'
+  get 'recipes', to: 'articles#recipes'
+  get 'advices', to: 'articles#advices'
+  get 'donate', to: 'articles#donate'
 end
